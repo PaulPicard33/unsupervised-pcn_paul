4874bb27137730dc3f4649ca17db92465a402c8e
PaulPicard33 <paulpicard33@gmail.com>
2026-08-11 16:31:07 +0200

    Update eval_claude.py

--- src/pcn/eval_claude.py
+++ src/pcn/eval_claude.py
@@ -137,7 +137,7 @@ def evaluate_generation(model, device, cf, nm_classes=10):
     # Assignation
     model.vodes[0].h = x_label
     model.vodes[-1].h = y_image_dummy
-    
+    model.init_ff(x_label, y_image_dummy, is_up=False)
     # Inférence pour générer l'image depuis le label
     model.infer(
         x_label=x_label, 
