1e50fc8eaec277b5a663da0a2f2ed3405530eb10
PaulPicard33 <paulpicard33@gmail.com>
2026-08-18 14:59:00 +0200

    Update eval_bPC.sh

--- hpc/eval_bPC.sh
+++ hpc/eval_bPC.sh
@@ -70,9 +70,8 @@ export LD_LIBRARY_PATH="/home/ppicard/.conda/envs/torch_env/lib:$LD_LIBRARY_PATH
 
 # 3. LE BOUCLIER VRAM (Indispensable car eval_claude.py importe les datamodules)
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-export JAX_PLATFORM_NAME=cpu
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform
 export TF_FORCE_GPU_ALLOW_GROWTH=true
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 # Explication : Exécution du script d'entraînement. 
 # L'option '-u' (unbuffered) est cruciale sur HPC : elle permet d'écrire les 'print' instantanément dans le fichier log au lieu d'attendre la fin de l'exécution.
 python -u src/pcn/eval_claude.py --model_path models/bpc-CIFAR10-T=32-ep=50.pt
